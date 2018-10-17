8fe8e19f5a846ce31716a2027db2097738bf0d77
Kevin Scott <kevin@hitheory.com>
2018-10-17 18:46:28 -0400

    Example of failing FormData with enzyme

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.formRef = React.createRef();
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.handleSubmit();
+    }, 1000);
+  }
+
+  handleSubmit = () => {
+    if (this.formRef.current) {
+      const formData = new FormData(this.formRef.current);
+      for (var [key, value] of formData.entries()) {
+        const r = [key, value];
+        console.log('return', r);
+        return r;
+      }
+    }
+
+    return null;
+  }
+
   render() {
+    /* failing to set a value fails the test */
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form ref={this.formRef}>
+          <input type="text" name="foo" />
+        </form>
       </div>
     );
+
+    /* setting an explicit value passes the test */
+    // return (
+    //   <div className="App">
+    //     <form ref={this.formRef}>
+    //       <input type="text" name="foo" defaultValue="foo-bar" />
+    //     </form>
+    //   </div>
+    // );
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+require('formdata-polyfill');
+
+it('should call fetch', () => {
+  const comp = (
+    <App />
+  );
+  const wrapper = mount(comp);
+  const instance = wrapper.instance();
+
+  expect(instance.handleSubmit()).toEqual([
+    'foo', 'foo-bar',
+  ]);
 });
